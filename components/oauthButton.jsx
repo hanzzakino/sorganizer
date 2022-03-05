d06d97b2735b20d584771e6c9538d15f3d8134b2
hanzzakino <emperornezl50@gmail.com>
2022-03-05 19:07:47 +0800

    Added Google OAuth

--- components/oauthButton.jsx
+++ components/oauthButton.jsx
@@ -1,3 +1,9 @@
+import { useRouter } from 'next/router'
+import {getAuth, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+import {db} from '../firebase.config'
+import { setDoc, doc, getDoc, serverTimestamp} from 'firebase/firestore'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 //Context
 import {useAuth} from '../context/AuthUserContext'
@@ -6,9 +12,35 @@ import {useTheme} from '../context/ThemeContext'
 
 export default function OauthButton() {
   const {theme, setTheme} = useTheme()
+  const router = useRouter()
+  const googleOauth = async () => {
+    try {
+    	const auth = getAuth()
+    	const provider = new GoogleAuthProvider()
+    	const result = await signInWithPopup(auth, provider)
+    	const user = result.user
 
-  const googleOauth = () => {
-    console.log('Google')
+    	const docRef = doc(db, 'users', user.uid)
+    	const docSnap = await getDoc(docRef)
+
+    	if(!docSnap.exists()){
+    		await setDoc(doc(db, 'users', user.uid), {
+    			firstname : user.displayName.split(' ').pop().join(),
+				lastname : user.displayName.split(' ')[1],
+				email : user.email,
+				timestamp : serverTimestamp()
+    		})
+    	}
+    	router.push('/user')
+    } catch(e) {
+    	toast('Could not sign in with Google', {
+						position : 'top-right',
+						autoClose : 5000,
+						theme : theme,
+						type : 'error',
+						hideProgressBar : true
+		})
+    }
   }
 
   return (
