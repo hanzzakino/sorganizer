75c9be44f343edc95920c52da1204c6bf649e0ec
hanzzakino <emperornezl50@gmail.com>
2022-03-15 14:18:57 +0800

    Fixed mobile view and added some styles

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -26,28 +26,28 @@ export default function Navbar({theme, collapsed}) {
 	</div>
 
 	<div className={collapsed ? 'navbar-menulist-collapsed bottom-line flex':'navbar-menulist bottom-line flex'}>
-		<span >
+		<span>
 			<i className={'bi bi-grid navbar-menulist-btn-icon '+theme+'-fgcolor'} />
-			<button className={collapsed ? theme+'-fgcolor navbar-menulist-btn-collapsed':theme+'-fgcolor navbar-menulist-btn'} onClick={() => setDashboardView('subjects')}>Subjects</button>
+			<button className={(collapsed ? theme+'-fgcolor navbar-menulist-btn-collapsed ':theme+'-fgcolor navbar-menulist-btn ')+(currentView==='subjects' ? 'navbar-btn-selected':'')} onClick={() => setDashboardView('subjects')}>Subjects</button>
 		</span>
 		<span>
 			<i className={'bi bi-card-checklist navbar-menulist-btn-icon '+theme+'-fgcolor'} />
-			<button className={collapsed ?  theme+'-fgcolor navbar-menulist-btn-collapsed': theme+'-fgcolor navbar-menulist-btn'} onClick={() => setDashboardView('todo')}>To&#8211;do</button>
+			<button className={(collapsed ?  theme+'-fgcolor navbar-menulist-btn-collapsed ': theme+'-fgcolor navbar-menulist-btn ')+(currentView==='todo' ? 'navbar-btn-selected':'')} onClick={() => setDashboardView('todo')}>To&#8211;do</button>
 		</span>
 		<span>
 			<i className={'bi bi-calendar-week navbar-menulist-btn-icon '+theme+'-fgcolor'} />
-			<button className={collapsed ?  theme+'-fgcolor navbar-menulist-btn-collapsed': theme+'-fgcolor navbar-menulist-btn'} onClick={() => setDashboardView('schedule')}>Schedule</button>
+			<button className={(collapsed ?  theme+'-fgcolor navbar-menulist-btn-collapsed ': theme+'-fgcolor navbar-menulist-btn ')+(currentView==='schedule' ? 'navbar-btn-selected':'')} onClick={() => setDashboardView('schedule')}>Schedule</button>
 		</span>
 		<span>
 			<i className={'bi bi-journal navbar-menulist-btn-icon '+theme+'-fgcolor'} />
-			<button className={collapsed ?  theme+'-fgcolor navbar-menulist-btn-collapsed': theme+'-fgcolor navbar-menulist-btn'} onClick={() => setDashboardView('notes')}>Notes</button>
+			<button className={(collapsed ?  theme+'-fgcolor navbar-menulist-btn-collapsed ': theme+'-fgcolor navbar-menulist-btn ')+(currentView==='notes' ? 'navbar-btn-selected':'')} onClick={() => setDashboardView('notes')}>Notes</button>
 		</span>
 	</div>
 	<br />
 	<div className={collapsed ? 'navbar-menulist-collapsed bottom-line flex':'navbar-menulist bottom-line flex'}>
 		<span>
 			<i className={'bi bi-gear navbar-menulist-btn-icon '+theme+'-fgcolor'} />
-			<button className={collapsed ?  theme+'-fgcolor navbar-menulist-btn-collapsed': theme+'-fgcolor navbar-menulist-btn'} onClick={() => setDashboardView('settings')}>Settings</button>
+			<button className={(collapsed ?  theme+'-fgcolor navbar-menulist-btn-collapsed ': theme+'-fgcolor navbar-menulist-btn ')+(currentView==='settings' ? 'navbar-btn-selected':'')} onClick={() => setDashboardView('settings')}>Settings</button>
 		</span>
 		<span>
 			<i className={'bi bi-'+(theme==='dark' ? 'moon':'sun')+' navbar-menulist-btn-icon-animated-'+theme+' '+theme+'-fgcolor'} />
