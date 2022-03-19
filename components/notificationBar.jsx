8ebb7faca8c2e42e23c5f9d54106cbcb19b17e81
hanzzakino <emperornezl50@gmail.com>
2022-03-19 23:24:29 +0800

    Styled the notification bar

--- components/notificationBar.jsx
+++ components/notificationBar.jsx
@@ -5,18 +5,26 @@ import Image from 'next/image'
 
 export default function NotificationBar({theme, scrolled, navbarCollapsed, userData, panelTitle, authUser}) {
 
+	const userClick = () => {
+		console.log('route to user')
+	}
+
 	return (
 	<>
 		<div className={'notificationbar '+(navbarCollapsed ? 'nb_collapsed ':' ')+(scrolled ? ' nb_scrolled':'')}></div>	
 		<div className={'notificationbar-top '+(navbarCollapsed ? 'nbt_collapsed':'')}>
-			<div className={'notificationbar-contents '+(scrolled ? 'nbc_scrolled':'')}>
+			<div className={'dark-fgcolor notificationbar-contents '+(scrolled ? 'nbc_scrolled':'')}>
 				<span>
-					<p>{panelTitle}</p>
+					<p className='notificationbar-title'>{panelTitle}</p>
 				</span>
 				 
-				<span className={'notificationbar-user-btn '+(navbarCollapsed ? 'nbub_collapsed':'')}>
-					<p>Hi!, {userData.firstname+' '+userData.lastname} &nbsp;&nbsp;&nbsp;&nbsp;</p>
-					<img src={authUser.photoURL} alt='Profile Picture' width={42} height={42} layout='fill'/>
+				<span onClick={userClick} className={'notificationbar-user-btn '+(navbarCollapsed ? 'nbub_collapsed':'')}>
+					<p className='notificationbar-username'>
+						{userData.firstname.length < 14 ? userData.firstname:userData.firstname.slice(0,11)+'...'}
+						&nbsp;&nbsp;&nbsp;&nbsp;
+					</p>
+
+					{authUser.photoURL===null ? <div className='notificationbar-profilepic' width={42} height={42}></div>:<img className='notificationbar-profilepic' src={authUser.photoURL} alt='Profile Picture' width={42} height={42} layout='fill'/>}
 				</span>
 			</div>
 		</div>	
